0a5f7ef8473343f2cfa17867ae7d909fe51e748c
kennethtxytqw <kennethtxytqw@gmail.com>
2016-09-27 16:28:30 +0800

    Added some comments

--- src/seedu/addressbook/storage/Storage.java
+++ src/seedu/addressbook/storage/Storage.java
@@ -25,8 +25,19 @@ public interface Storage {
         }
     }
     
+    /**
+     * Loads data from this storage file.
+     *
+     * @throws StorageOperationException if there were errors reading and/or converting data from file.
+     */
     public AddressBook load() throws StorageOperationException;
     public String getPath();
+    
+    /**
+     * Saves all data to this storage file.
+     *
+     * @throws StorageOperationException if there were errors converting and/or storing data to file.
+     */
     public void save(AddressBook addressBook) throws StorageOperationException;
     
 }
